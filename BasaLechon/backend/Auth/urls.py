19b631373cc46b552df2a6e0b35c3079c6e4b6e7
Juan Carlos Miguel T. Basa <143696331+jayceebasa@users.noreply.github.com>
2025-03-18 15:24:24 +0800

    push

--- BasaLechon/backend/Auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.urls import include
-
-urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/google/', include('allauth.socialaccount.urls')),
-]
